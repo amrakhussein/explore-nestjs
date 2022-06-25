d12ebf76aba8a589ad8b27b6ac1296ad337db0da
Amro Abdelkamel <amromoorie@gmail.com>
2022-06-25 13:05:23 +0200

    Feat(): add logger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  await app.listen(6000);
+  const PORT = 6000;
+  await app.listen(PORT);
+  logger.log(`App is running on PORT ${PORT}`);
 }
 bootstrap();
